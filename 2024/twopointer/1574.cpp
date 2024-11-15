a7c980a0d941b6845090667f32aacb70632a1b36
Haoyang Chen <haoychen@outlook.com>
2024-11-14 22:10:23 -0800

    Create 1574.cpp

---
+++ 2024/twopointer/1574.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& arr) {
+        int r = arr.size() - 1;
+        while (r >= 1 && arr[r-1] <= arr[r]) r--;
+        
+        int res = r;
+        int l = 0;
+        while (l < r && (l == 0 || arr[l - 1] <= arr[l])) {
+            while (r < arr.size() && arr[l] > arr[r]) r++;
+            res = min(res, r - l - 1);
+            l++;
+        }
+        return res;
+    }
+};
