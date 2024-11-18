d8823af8d4bc88caee436e1dcfbcc2b177a4798c
Haoyang Chen <haoychen@outlook.com>
2024-11-17 17:38:44 -0800

    Create 1652.cpp
    not elegant, too many corner cases

---
+++ 2024/slidingwindow/1652.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> prefix(n + 1, 0);
+        for (int i = 1; i <= n; i++) prefix[i] = prefix[i-1] + code[i-1];
+        
+        vector<int> res(n, 0);
+        if (k == 0) return res;
+        for (int i = 1; i <= n; i++) {
+            int s = 0, e = 0;
+            if (k > 0) s = i + 1, e = i + k;
+            else s = i - abs(k), e = i - 1;
+            cout << s << ',' << e << endl;
+            if (s > 0 && e <= n) {
+                res[i-1] = prefix[e] - prefix[s-1];
+            } else if (s <= 0 && e > 0) {
+                res[i-1] = prefix[n] - prefix[n+s-1] + prefix[e];
+            } else if (s <= 0 && e <= 0) {
+                res[i-1] = prefix[n+e] - prefix[n+s-1];
+            } else if (s <= n && e > n) {
+                res[i-1] = prefix[n] - prefix[s-1] + prefix[e-n];
+            } else {
+                res[i-1] = prefix[e-n] - prefix[s-n-1];
+            }
+        }
+
+        return res;
+    }
+};
