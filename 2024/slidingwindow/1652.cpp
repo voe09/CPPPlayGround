4353554abd5ed49ea3daffdbc7b65c6db05b6332
Haoyang Chen <haoychen@outlook.com>
2024-11-17 17:41:49 -0800

    Update 1652.cpp
    good rolling window solution

--- 2024/slidingwindow/1652.cpp
+++ 2024/slidingwindow/1652.cpp
@@ -28,3 +28,29 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> res(n, 0);
+        if (k == 0) return res;
+
+        int start = 1, end = k, sum = 0;
+        if (k < 0) {
+            start = n - abs(k);
+            end = n - 1;
+        }
+
+        for (int i = start; i <= end; i++)  sum += code[i];
+        for (int i = 0; i < n; i++) {
+            res[i] = sum;
+            sum -= code[start % n];
+            sum += code[(end + 1) % n];
+            start++, end++;
+        }
+
+        return res;
+    }
+};
